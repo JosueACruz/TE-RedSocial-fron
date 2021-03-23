b077e0f05a271bb2ed285546fe133fab18998edd
Josue Cruz <josuealfredo3@gmail.com>
2021-03-22 22:47:05 -0600

    Estructura del proyecto, bootstrap instalado, jquery, Navbar, login y registrarse aun sin terminar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//Aqui esta llamando los modulos
+const routes: Routes = [
+  //creo una ruta
+  //Con esto lo que hago es que al entrar al localhost:2400 de neustro server
+  //nos redireccione automaticamente al modulo o componente home
+  {
+    path:"",
+    redirectTo:"/home",
+    pathMatch:"full",
+  },
+
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, 
+  { path: 'login', loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule) }, 
+  { path: 'register', loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
