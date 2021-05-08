dd36c7fdfab703c9e9fe92d12b3840d76a12c43a
cerritos <mu.10cerritos@gmail.com>
2021-05-07 23:38:23 -0600

    creando servicio para login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RestService } from './rest.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redsocial-front';
+  constructor(private restService:RestService){
+
+  }
+
+  ngOnInit(): void {
+    this.cargarData();
+  }
+
+  public cargarData(){
+    this.restService.get('http://127.0.0.1:8000/api/usuarios')
+    .subscribe(respuesta =>{
+      console.log(respuesta); 
+    })
+  }
 }
